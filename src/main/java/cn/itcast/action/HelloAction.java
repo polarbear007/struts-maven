a4a5ce2b88a91d7e98d11e6e6a275f460bfee363
eric <1479124685@qq.com>
2019-03-08 05:23:38 +0800

    以前没有使用maven 构建，现在补充一下，jar包什么的非常方便

---
+++ src/main/java/cn/itcast/action/HelloAction.java
@@ -0,0 +1,11 @@
+package cn.itcast.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloAction extends ActionSupport {
+	private static final long serialVersionUID = 1L;
+	
+	public String test() {
+		return "success";
+	}
+}
